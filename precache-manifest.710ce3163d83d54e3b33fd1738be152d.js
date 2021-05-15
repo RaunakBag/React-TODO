96ab2d2b1070a0b703272392c3fb3a5234632f1a
RaunakBag <raunakbag97@gmail.com>
2021-05-15 11:30:17 +0530

    Updates

---
+++ precache-manifest.710ce3163d83d54e3b33fd1738be152d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd48e72637dbb7aee73b86c5796c9bd",
+    "url": "/React-TODO/index.html"
+  },
+  {
+    "revision": "0b8d281b39752cf852ad",
+    "url": "/React-TODO/static/css/main.dad1c352.chunk.css"
+  },
+  {
+    "revision": "a2fabf7406b960bd42ff",
+    "url": "/React-TODO/static/js/2.ffaef20f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-TODO/static/js/2.ffaef20f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8d281b39752cf852ad",
+    "url": "/React-TODO/static/js/main.ac83a0f5.chunk.js"
+  },
+  {
+    "revision": "4c2d08cbc3edadfffce2",
+    "url": "/React-TODO/static/js/runtime-main.907af8ba.js"
+  }
+]);
